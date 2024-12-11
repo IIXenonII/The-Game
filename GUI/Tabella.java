5d52193e26c1bc91a9e49d764ee46753405f31d0
baran <barandeep05@gmail.com>
2024-12-11 17:44:41 +0100

    Started GUI

--- GUI/Tabella.java
+++ GUI/Tabella.java
@@ -1,15 +1,81 @@
 package GUI;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
 
 public class Tabella {
     private JFrame frameGioco;
-
-    //private Dimensione bordoEsterno = new Dimensione(600, 600); 
+    private static DimensionUIResource bordoEsterno = new DimensionUIResource(600, 600)  ; 
 
     public Tabella() {
         this.frameGioco = new JFrame("Scacchi");
-        this.frameGioco.setSize(null);
+        JMenuBar barraMenu = inizializzaBarraMenu();
+        
+        this.frameGioco.setSize(bordoEsterno);
         this.frameGioco.setVisible(true);
+        this.frameGioco.setJMenuBar(barraMenu);
     }
+
+    private JMenuBar inizializzaBarraMenu() {
+        JMenuBar barraMenu = new JMenuBar();
+        barraMenu.add(creaFileMenu());
+        return barraMenu;
+    }
+
+    private JMenu creaFileMenu() {
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem apriPNG = new JMenuItem("Load PGN file"); 
+        apriPNG.addActionListener(new Action() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Apri PGN");
+            }
+
+            @Override
+            public Object getValue(String key) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'getValue'");
+            }
+
+            @Override
+            public void putValue(String key, Object value) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'putValue'");
+            }
+
+            @Override
+            public void setEnabled(boolean b) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'setEnabled'");
+            }
+
+            @Override
+            public boolean isEnabled() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener listener) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'addPropertyChangeListener'");
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener listener) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'removePropertyChangeListener'");
+            }
+        });
+        fileMenu.add(apriPNG);
+        return fileMenu;
+    }
+
+
+
+
 }
--- testScac.java
+++ testScac.java
@@ -1,7 +1,11 @@
+import GUI.Tabella;
+
 public class testScac {
     public static void main(String[] args) {
         Scacchiera s = Scacchiera.scacchieraStandard();
 
         System.out.println(s);
+
+        Tabella t = new Tabella();
     }
 }
