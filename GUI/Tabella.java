73d077a4f2ab03ac50b049701817dc8c0abadf72
baran <barandeep05@gmail.com>
2024-12-11 21:10:38 +0100

    added arte and changed the logic of the Table class

--- GUI/Tabella.java
+++ GUI/Tabella.java
@@ -1,28 +1,47 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+
+import GUI.Tabella;
+import GameSide.Scacchiera;
+
 
 public class Tabella {
     private JFrame frameGioco;
     private static Dimension bordoEsterno = new Dimension(600, 600); 
     private PanelloScacchiera panelloScacchiera;
+    public Scacchiera scacchiera;
+    String IconaPezzo="";
+    
+    public Scacchiera getScacchiera() {
+        return scacchiera;
+    }
+
 
     public Tabella() {
         this.frameGioco = new JFrame("Scacchi");
         this.frameGioco.setLayout(new BorderLayout());
         JMenuBar barraMenu = inizializzaBarraMenu();
         this.frameGioco.setJMenuBar(barraMenu);
+        this.scacchiera = Scacchiera.scacchieraStandard();
         this.frameGioco.setSize(bordoEsterno);
-
         this.panelloScacchiera = new PanelloScacchiera();
         this.frameGioco.add(this.panelloScacchiera, BorderLayout.CENTER );
-
         this.frameGioco.setVisible(true);
         
     }
@@ -60,6 +79,65 @@ public class Tabella {
     }
 
 
+    public class PanelloScacchiera extends JPanel{
+        private List<PanelloTile> caselleScacchiera;
+        private Dimension dimensioneScacchiera= new Dimension(400, 350);
+
+        PanelloScacchiera(){
+            super(new GridLayout(8, 8));
+            this.caselleScacchiera = new ArrayList<>();
+            for (int i = 0; i < 64; i++) {
+                PanelloTile panelloTile = new PanelloTile(this, i);
+                caselleScacchiera.add(panelloTile);
+                // Lo mette nella gui
+                add(panelloTile);
+            }
+            setPreferredSize(dimensioneScacchiera);
+            validate();
+        }
+    
+    }
+
+    private class PanelloTile extends JPanel{
+        private Color chiaro = Color.decode("#FFFACD");
+        private Color scuro = Color.decode("#593E1A");;
+        private Dimension dimensioneTile =  new Dimension(10, 10);
+        private int tileID;
+        PanelloTile(PanelloScacchiera panelloScacchiera, int tileID){
+            super(new GridBagLayout());
+            this.tileID = tileID;
+            setPreferredSize(dimensioneTile);
+            assegnaColore();
+            assegnaIconaPezzo(scacchiera);
+            validate();
+
+        }
+
+        private void assegnaIconaPezzo(Scacchiera scacchiera){
+            this.removeAll();
+            if (scacchiera.getTile(this.tileID).isThere()) {
+                
+                try {
+                    BufferedImage imagine = 
+                        ImageIO.read(new File(IconaPezzo+ scacchiera.getTile(this.tileID).getPezzo().getColorePezzo().toString().substring(0,1) + 
+                        scacchiera.getTile(this.tileID).getPezzo().toString() + ".gif"));
+                    this.add(new JLabel(new ImageIcon(imagine)));
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+        }
+
+        private void assegnaColore() {
+            boolean isLight = ((this.tileID + this.tileID / 8) % 2 == 0);
+            setBackground(isLight ? chiaro : scuro);
+        }
+            
+    }
+
 
 
 }
+
+
